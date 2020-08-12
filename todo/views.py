0d0b21038da7479e74899bb5d5d8b1be51b5dad7
swiftvic <victor@maratech.ca>
2020-08-12 12:35:01 -0400

    added viewtodos, updatetodos, completetodos

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -64,3 +65,29 @@ def createtodo(request):
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos':todos})
+
+def viewtodo(request, todo_pk):
+    # Assign todo as an object from Todo model, pk and user=the requested user
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    # If just displaying the data GET
+    if request.method == "GET":
+        form = TodoForm(instance=todo)
+        return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form})
+    # If data is edited and POST is submitted
+    else:
+        try:
+            # Assign form with the POSTed updates and update the pk record todo
+            form = TodoForm(request.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            # If length of text sumbitted is more than Chars specified in models
+            return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Incorrect data passed in. Try again.'} )
+
+def completetodo(request, todo_pk):
+    # Assign todo as an object from Todo model, pk and user=the requested user
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now() # Set the datecompleted in models to have a time
+        todo.save()
+        return redirect('currenttodos')
