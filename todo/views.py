a5edc1df295163076dada638892df6df91ec3d9e
swiftvic <victor@maratech.ca>
2020-08-12 15:07:47 -0400

    Added listing compeleted todos, login required decorator

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'todo/home.html')
@@ -43,11 +44,13 @@ def loginuser(request):
             # Not authenticated
             return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error':'Username/Password does not match'} )
 
+@login_required
 def logoutuser(request):
     if request.method == "POST":
         logout(request)
         return redirect('home')
 
+@login_required
 def createtodo(request):
     if request.method == "GET":
         return render(request, 'todo/createtodo.html', {'form':TodoForm()})
@@ -62,10 +65,12 @@ def createtodo(request):
             # If length of text sumbitted is more than Chars specified in models
             return render(request, 'todo/createtodo.html', {'form':TodoForm(), 'error':'Incorrect data passed in. Try again.'} )
 
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos':todos})
 
+@login_required
 def viewtodo(request, todo_pk):
     # Assign todo as an object from Todo model, pk and user=the requested user
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
@@ -84,6 +89,7 @@ def viewtodo(request, todo_pk):
             # If length of text sumbitted is more than Chars specified in models
             return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Incorrect data passed in. Try again.'} )
 
+@login_required
 def completetodo(request, todo_pk):
     # Assign todo as an object from Todo model, pk and user=the requested user
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
@@ -91,3 +97,16 @@ def completetodo(request, todo_pk):
         todo.datecompleted = timezone.now() # Set the datecompleted in models to have a time
         todo.save()
         return redirect('currenttodos')
+
+@login_required
+def deletetodo(request, todo_pk):
+    # Assign todo as an object from Todo model, pk and user=the requested user
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos':todos})
